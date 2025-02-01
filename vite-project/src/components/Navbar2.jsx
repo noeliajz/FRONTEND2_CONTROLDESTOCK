3803a27a2379ebb8a70d0a859042f3b01d11f1f9
noeliajudithzelaya <noeliajudithzelayautnfrt@gmail.com>
2025-02-01 18:24:38 -0300

    se crea el formulario de login

--- vite-project/src/components/Navbar2.jsx
+++ vite-project/src/components/Navbar2.jsx
@@ -9,15 +9,15 @@ const Navbar2 = () => {
     <>
     <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="/">Control de stock</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#features">Inicio</Nav.Link>
+            <Nav.Link href="/">Inicio</Nav.Link>
             <Nav.Link href="#pricing">Quienes somos</Nav.Link>
             <Nav.Link href="#pricing">Contacto</Nav.Link>
             <Nav.Link href="#pricing">Registro</Nav.Link>
-            <Nav.Link href="#pricing">Login</Nav.Link>
+            <Nav.Link href="/Login">Login</Nav.Link>
         </Nav>
         </Navbar.Collapse>
       </Container>
